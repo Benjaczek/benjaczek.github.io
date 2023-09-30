94db5e12c2be3204018dbdc5c5e7166b95c0809f
Benjaczek <146527541+Benjaczek@users.noreply.github.com>
2023-09-30 14:44:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 // Get all menu links
 const menuLinks = document.querySelectorAll('.menu-link');
 
+let touchedLinks = {};
+
 // Add click event listeners to each menu link
 menuLinks.forEach((menuLink) => {
     menuLink.addEventListener('click', (event) => {
-        event.preventDefault(); // Prevent the default link behavior
-
-        // Get the href attribute of the clicked link
         const href = menuLink.getAttribute('href');
 
-        // Check if the current page matches the link's href
-        if (window.location.href.endsWith(href)) {
-            // If it matches, go back to the index page
-            window.location.href = 'index.html';
+        if (isMobileDevice()) {
+            // If the link hasn't been touched yet, show the dropdown or hover effect
+            if (!touchedLinks[href]) {
+                event.preventDefault();
+
+                // Add your dropdown/hover effect here
+                // For example: menuLink.classList.add('hover-effect');
+
+                // Mark this link as touched
+                touchedLinks[href] = true;
+            } else {
+                // If the link has been touched once, then navigate
+                navigateToLink(href);
+            }
         } else {
-            // If it doesn't match, go to the link's href
-            window.location.href = href;
+            // Not on a mobile device, just navigate
+            navigateToLink(href);
         }
     });
 });
+
+function navigateToLink(href) {
+    if (window.location.href.endsWith(href)) {
+        window.location.href = 'index.html';
+    } else {
+        window.location.href = href;
+    }
+}
+
+function isMobileDevice() {
+    return (window.innerWidth <= 800) && (window.innerHeight <= 600); 
+    // Adjust the values to suit your requirements
+}
