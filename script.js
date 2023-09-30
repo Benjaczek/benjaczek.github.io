e0d7ea5f166ab4e2ae2f566d6bdc7a790fc86b6b
Benjaczek <146527541+Benjaczek@users.noreply.github.com>
2023-09-30 16:45:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -37,33 +37,51 @@ function isMobileDevice() {
     // Adjust the values to suit your requirements
 }
 
+// These are the new functions and event listeners
+function hideAllInfoBoxes() {
+    const infoBoxes = document.querySelectorAll('.info-box');
+    infoBoxes.forEach(box => {
+        box.style.display = 'none';
+    });
+}
 
-document.getElementById('productions').addEventListener('click', function() {
-    const infoBox = document.getElementById('productions-info');
-    
+function toggleInfoBox(id) {
+    const infoBox = document.getElementById(id);
     if (infoBox.style.display === 'none' || infoBox.style.display === '') {
         infoBox.style.display = 'block';
     } else {
         infoBox.style.display = 'none';
     }
+}
+
+function hideAllInfoBoxes() {
+    const infoBoxes = document.querySelectorAll('.info-box');
+    infoBoxes.forEach(box => {
+        box.classList.remove('showing');
+    });
+}
+
+function toggleInfoBox(id) {
+    const infoBox = document.getElementById(id);
+    if (!infoBox.classList.contains('showing')) {
+        infoBox.classList.add('showing');
+    } else {
+        infoBox.classList.remove('showing');
+    }
+}
+
+
+document.getElementById('productions').addEventListener('click', function() {
+    hideAllInfoBoxes();
+    toggleInfoBox('productions-info');
 });
 
 document.getElementById('compositions').addEventListener('click', function() {
-    const infoBox = document.getElementById('compositions-info');
-    
-    if (infoBox.style.display === 'none' || infoBox.style.display === '') {
-        infoBox.style.display = 'block';
-    } else {
-        infoBox.style.display = 'none';
-    }
+    hideAllInfoBoxes();
+    toggleInfoBox('compositions-info');
 });
 
 document.getElementById('more').addEventListener('click', function() {
-    const infoBox = document.getElementById('more-info');
-    
-    if (infoBox.style.display === 'none' || infoBox.style.display === '') {
-        infoBox.style.display = 'block';
-    } else {
-        infoBox.style.display = 'none';
-    }
+    hideAllInfoBoxes();
+    toggleInfoBox('more-info');
 });
